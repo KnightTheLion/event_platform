cba3e8478050a72368aeedbacc3ffb8bd10441eb
KnightTheLion <joshuaknightmitchell@gmail.com>
2023-12-22 19:22:51 -0500

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={poppins.variable}>{children}</body>
+        <SpeedInsights/>
       </html>
     </ClerkProvider>
   );
