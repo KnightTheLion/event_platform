6ef27dea0b504722015beea65d7ae238f45e0bcf
KnightTheLion <joshuaknightmitchell@gmail.com>
2023-12-22 23:54:27 -0500

    checking this solution as well?

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,6 +18,7 @@ export const formatDateTime = (dateString: Date) => {
     hour: 'numeric', // numeric hour (e.g., '8')
     minute: 'numeric', // numeric minute (e.g., '30')
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+    timeZone: `America/New_York`, 
   }
 
   const dateOptions: Intl.DateTimeFormatOptions = {
@@ -25,6 +26,7 @@ export const formatDateTime = (dateString: Date) => {
     month: 'short', // abbreviated month name (e.g., 'Oct')
     year: 'numeric', // numeric year (e.g., '2023')
     day: 'numeric', // numeric day of the month (e.g., '25')
+    timeZone: `America/New_York`, 
   }
 
   
